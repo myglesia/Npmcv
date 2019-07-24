682aecfd3c069d6bdad19d3c851138ebc071a1d0
myglesia <myglesia@uoregon.edu>
2019-07-24 12:36:50 -0700

    Added timer

--- npmcv/__main__.py
+++ npmcv/__main__.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
 import sys
+import time
 from npmcv import npmcv
-__version__ = '0.3.0'
+
 
 def run():
+    t = time.process_time()
     for arg in sys.argv[1:]:
         npmcv.main(arg)
+    elapsed_time = (time.process_time() - t) / 60
+    print('\x1b[1;32m' +
+          'Total Runtime: {:.4} mins.'.format(elapsed_time) + '\x1b[0m')
+
 
 if __name__ == '__main__':
-	run()
+    run()

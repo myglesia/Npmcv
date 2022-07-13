c2ff0f6f3ec83995281cd9a7026ee8dc9a792b6b
myglesia <myglesia@uoregon.edu>
2022-07-13 15:22:52 -0700

    PEP8 formatting, renaming module

--- src/npmcv/__main__.py
+++ src/npmcv/__main__.py
@@ -2,22 +2,29 @@
 
 import time
 import argparse
-import Npmcv
+import npmcv
+
 
 def run():
     # Argparse
     parser = argparse.ArgumentParser(prog='npmcv')
-    parser.add_argument('-o', '--output', default=None, type=str, nargs='?', help='output file name',)
-    parser.add_argument('-V', '--version', action='version', version='npmcv version: {}'.format(Npmcv.__version__))
-    parser.add_argument('path', type=str, help='directory containing lif images', metavar='<path>')
+    parser.add_argument('-D', '--no_imgs', action='store_false',
+                        help='Don\'t save overlays and individual cell images')
+    parser.add_argument('-n', '--name', default=None, type=str,
+                        nargs='?', help='Set basename for the output files')
+    parser.add_argument('-V', '--version', action='version',
+                        version='npmcv version: {}'.format(npmcv.__version__))
+    parser.add_argument('path', type=str, metavar='<path>',
+                        help='directory containing lif images')
+
     args = parser.parse_args()
 
-
     t = time.process_time()
-    Npmcv.main(vars(args))
+    npmcv.main(vars(args))
     elapsed_time = (time.process_time() - t) / 60
     print('\x1b[1;32m' +
           'Total Runtime: {:.4} mins.'.format(elapsed_time) + '\x1b[0m \n')
+
 
 if __name__ == '__main__':
     run()
